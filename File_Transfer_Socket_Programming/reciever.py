7898898873152b19ac0916e43d8cf1ef3bb67c65
Malay Bhavsar <55908198+Leo-Malay@users.noreply.github.com>
2020-12-29 19:37:19 +0530

    Add files via upload

--- File_Transfer_Socket_Programming/reciever.py
+++ File_Transfer_Socket_Programming/reciever.py
@@ -1,11 +1,12 @@
 # This file will be used for recieving files over socket connection.
 import os
 import socket
+import time
 
 host = input("Host Name: ")
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Trying to connect to socket.
 try:
     sock.connect((host, 22222))
     print("Connected Successfully")
@@ -13,14 +14,28 @@ except:
     print("Unable to connect")
     exit(0)
 
+# Send file details.
 file_name = sock.recv(100).decode()
 file_size = sock.recv(100).decode()
+
+# Opening and reading file.
 with open("./rec/" + file_name, "wb") as file:
     c = 0
-    print("Recieving file")
-    while c != file_size:
+    # Starting the time capture.
+    start_time = time.time()
+
+    # Running the loop while file is recieved.
+    while c <= int(file_size):
         data = sock.recv(1024)
+        if not (data):
+            break
         file.write(data)
         c += len(data)
-    print("File recieved")
+
+    # Ending the time capture.
+    end_time = time.time()
+
+print("File transfer Complete.Total time: ", end_time - start_time)
+
+# Closing the socket.
 sock.close()
--- File_Transfer_Socket_Programming/sender.py
+++ File_Transfer_Socket_Programming/sender.py
@@ -1,23 +1,42 @@
-# This file is used for sending the fileover socket
+# This file is used for sending the file over socket
 import os
 import socket
+import time
 
-file_name = input("File_name: ")
+# Creating a socket.
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((socket.gethostname(), 22222))
 sock.listen(5)
+print("Host Name: ", sock.getsockname())
 
+# Accepting the connection.
 client, addr = sock.accept()
 
-client.send(file_name.encode())
-client.send(os.path.getsize(file_name).encode())
-with open(file_name, "wb") as file:
-    c = 0
-    print("Sending file")
-    while c <= os.path.getsize(file_name):
-        data = file.read(1024)
-        client.send(data)
-        c += len(data)
-    print("File sent")
+# Getting file details.
+file_name = input("File Name:")
+file_size = os.path.getsize(file_name)
 
+# Sending file_name and detail.
+client.send(file_name.encode())
+client.send(str(file_size).encode())
+
+# Opening file and sending data.
+with open(file_name, "rb") as file:
+    c = 0
+    # Starting the time capture.
+    start_time = time.time()
+
+    # Running loop while c != file_size.
+    while c <= file_size:
+        data = file.read(1024)
+        if not (data):
+            break
+        client.sendall(data)
+        c += len(data)
+
+    # Ending the time capture.
+    end_time = time.time()
+
+print("File Transfer Complete.Total time: ", end_time - start_time)
+# Cloasing the socket.
 sock.close()
